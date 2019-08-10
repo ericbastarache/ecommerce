11f96bdc0bd8c18f3d2417fcb4a043d668646ec1
Eric Bastarache <eric_bastarache@hotmail.com>
2019-08-10 18:55:14 -0400

    React router implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
